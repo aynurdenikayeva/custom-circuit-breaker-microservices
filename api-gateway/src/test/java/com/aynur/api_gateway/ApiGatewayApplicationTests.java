a7411167924175aa492d9501ea704b2c6ee1f65a
Aynur <113580226+aynurdenikayeva@users.noreply.github.com>
2026-02-20 18:51:04 +0400

    Initial commit - Custom Circuit Breaker Microservices

---
+++ api-gateway/src/test/java/com/aynur/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aynur.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/aynur/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aynur.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
